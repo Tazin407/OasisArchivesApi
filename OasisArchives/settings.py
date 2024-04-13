aefdae5e92edd93cbe9fbea110d0c2359d50c51a
tazin407 <tazintasnim34@gmail.com>
2024-04-13 21:08:56 +0600

    Added CSRF

--- OasisArchives/settings.py
+++ OasisArchives/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS= ['https://oasisarchivesapi.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition

32ba5fcc898f20d8310a919f0b770d53e5034287
tazin407 <tazintasnim34@gmail.com>
2024-04-14 13:04:03 +0600

    added default renderer class

--- OasisArchives/settings.py
+++ OasisArchives/settings.py
@@ -63,6 +63,10 @@ REST_FRAMEWORK = {
     # # ],
     #  'DEFAULT_PERMISSION_CLASSES': (
     #     'rest_framework.permissions.IsAuthenticated', )
+     'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        
+    ],
 }
 
 MIDDLEWARE = [

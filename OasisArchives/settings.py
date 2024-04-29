b929cdac3507870ca0a71c2af5e4c42be286f669
tazin407 <tazintasnim34@gmail.com>
2024-04-29 21:16:37 +0600

    changed static

--- OasisArchives/settings.py
+++ OasisArchives/settings.py
@@ -165,15 +165,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-#added extra
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'images'),
-]
-
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'images')
-
-# URL used to access the media
 MEDIA_URL = '/media/'
 
 # Default primary key field type
